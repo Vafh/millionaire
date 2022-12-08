39dd5e62a2e0e7b83593ea8c002b854a57551275
Ivan_Markushyn <vanuhamark@gmail.com>
2022-12-08 22:47:05 +0200

    [1] fix routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { ErrorPage } from "./pages/errorPage";
 import { GameOverPage } from "./pages/gameOverPage";
@@ -6,14 +6,25 @@ import { HomePage } from "./pages/homePage";
 import { QuestionPage } from "./pages/questionPage";
 
 function App() {
-  return (
-    <Routes>
-      <Route path= "/" element={ <HomePage /> }/>
-      <Route path= "question/:questionNumber" element={ <QuestionPage /> }/>
-      <Route path= "end" element={ <GameOverPage /> }/>
-      <Route path= "*" element={ <ErrorPage /> }/>
-    </Routes>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "question/:questionNumber",
+      element: <QuestionPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "end",
+      element: <GameOverPage />,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
